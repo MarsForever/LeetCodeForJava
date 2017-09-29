009b03ff00865bfd898dc715f531cc2413708ef5
MarsForever <MarsForever@users.noreply.github.com>
2017-09-29 20:13:26 +0900

    Initial commit

---
+++ src/Solution657.java
@@ -0,0 +1,21 @@
+class Solution657 {
+	public boolean judgeCircle(String moves) {
+		if (moves == null) {
+			return true;
+		}
+		int x = 0, y = 0;
+		for (int i = 0; i < moves.length(); i++) {
+			char ch = moves.charAt(i);
+			if (ch == 'R') {
+				x++;
+			}else if (ch == 'L') {
+				x--;
+			}else if (ch == 'U') {
+				y++;
+			}else if (ch == 'D') {
+				y--;
+			}
+		}
+		return x == 0 && y == 0;
+	}
+}
---
+++ src/judgeRouteCircle657.java
@@ -0,0 +1,8 @@
+
+public class judgeRouteCircle657 {
+	public static void main(String[] args) {
+		String moves = "LD";
+		Solution657 a = new Solution657();
+		System.out.println(a.judgeCircle(moves));
+	}
+}
