9503b758e6d96d446ce026c4b520bb088cb4876d
MarsForever <wuguijiadeyu@gmail.com>
2018-10-18 21:54:36 +0900

    add 412 Fizz Buzz

---
+++ src/Execute/fizzBuzz412.java
@@ -0,0 +1,9 @@
+package Execute;
+
+public class fizzBuzz412 {
+	public static void main(String args[]) {
+		Solutions.Solution412 test = new Solutions.Solution412();
+		int n = 15;
+		System.out.println(test.fizzBuzz(n));
+	}
+}
