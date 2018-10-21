508d2731798bb3ee158f6ad919303752db5b8815
MarsForever <wuguijiadeyu@gmail.com>
2018-10-21 21:59:25 +0900

    add 728

---
+++ src/Execute/selfDividingNumbers728.java
@@ -0,0 +1,14 @@
+package Execute;
+
+public class selfDividingNumbers728 {
+	public static void main(String[] args) {
+		String s = "A man, a plan, a canal: Panama";
+		//input: hello 
+		//output:olleh
+		//input: A man, a plan, a canal: Panama
+		//output:amanaP :lanac a ,nalp a ,nam A
+
+		Solutions.Solution728 test = new Solutions.Solution728();
+		System.out.println(test.reverseString(s));
+	}
+}
