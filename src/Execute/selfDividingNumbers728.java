fd4c49596e16915eb4a608eeeeecf9d8516fdb1e
MarsForever <wuguijiadeyu@gmail.com>
2018-10-23 23:49:18 +0900

    add leetcode 728

--- src/Execute/selfDividingNumbers728.java
+++ src/Execute/selfDividingNumbers728.java
@@ -2,13 +2,14 @@ package Execute;
 
 public class selfDividingNumbers728 {
 	public static void main(String[] args) {
-		String s = "A man, a plan, a canal: Panama";
+		int left = 1;
+		int right = 22;
 		//input: hello 
 		//output:olleh
 		//input: A man, a plan, a canal: Panama
 		//output:amanaP :lanac a ,nalp a ,nam A
 
 		Solutions.Solution728 test = new Solutions.Solution728();
-		System.out.println(test.reverseString(s));
+		System.out.println(test.selfDividingNumbers(left, right));
 	}
 }
--- src/Solutions/Solution728.java
+++ src/Solutions/Solution728.java
@@ -1,7 +1,29 @@
 package Solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution728 {
-	public String reverseString(String s) {
-		return new StringBuilder(s).reverse().toString();
- 	}
+	public List<Integer> selfDividingNumbers(int left, int right){
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i = left ; i <= right ; i++) {
+			
+			String word = Integer.toString(i);
+			if(word.indexOf("0") >= 1) {
+				continue;
+			}
+			String []wordArray = word.split("");
+			int temp = 0;
+			for(int j = 0 ; j < wordArray.length ; j++) {
+				if(i % Integer.parseInt(wordArray[j]) == 0) {
+					temp++;
+				}
+			}
+			if(temp == wordArray.length) {
+				list.add(i);
+			}
+
+		}
+		return list;
+	}
 }
