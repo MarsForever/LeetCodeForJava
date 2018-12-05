d31c21a715d29da5586198b4802b8eb67e2079c8
MarsForever <wuguijiadeyu@gmail.com>
2018-12-05 23:57:15 +0900

    solution35 version2 4 ms, faster than 40.56%

--- src/Execute/searchInsertPosition35.java
+++ src/Execute/searchInsertPosition35.java
@@ -5,7 +5,7 @@ import Solutions.Solution35;
 public class searchInsertPosition35 {
 	public static void  main(String args[]) {
 		int[] nums = {1,3,5,6};
-		int target = 0;
+		int target = 2;
 		Solution35 test = new Solution35();
 		System.out.println(test.searchInsert(nums, target));
 	}
--- src/Solutions/Solution35.java
+++ src/Solutions/Solution35.java
@@ -1,35 +1,13 @@
 package Solutions;
 
 import java.util.stream.IntStream;
-//Runtime: 79 ms, faster than 0.93% of Java online submissions for Search Insert Position.
+//Runtime: 4 ms, faster than 40.56% of Java online submissions for Search Insert Position.
+//http://www.cnblogs.com/grandyang/p/4408638.html
 public class Solution35 {
 	public int searchInsert(int[] nums, int target) {
-		int low = 0;
-		int high = nums.length -1;
-		if(target < nums[0]) {
-			return 0;
+		for(int i = 0 ; i < nums.length ; i++) {
+			if(nums[i] >= target) return i;
 		}
-		if(target > nums[high]) {
-			return nums.length;
-		}
-		boolean contains  = IntStream.of(nums).anyMatch(x -> x == target);
-		if(contains) {
-			while (low <= high) {
-				int mid = (low + high) /2;
-				if (nums[mid] == target) {
-					return mid;
-				}else if(nums[mid] < target) {
-					low = mid + 1;
-				}else {
-					high = mid -1;
-				}
-			}
-		}
-		for(int i  = 0 ; i < nums.length ; i++) {
-			if(target > nums[i] && target < nums[i+1]) {
-				return i+1;
-			}
-		}
-		return target;
+		return nums.length;
     }
 }
