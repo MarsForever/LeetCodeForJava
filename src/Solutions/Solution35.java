228592ba2bf96ffa1618ebf8f8969f9865a43c90
MarsForever <wuguijiadeyu@gmail.com>
2018-12-07 22:34:15 +0900

    change right and while satement

--- src/Solutions/Solution35.java
+++ src/Solutions/Solution35.java
@@ -8,12 +8,12 @@ public class Solution35 {
 		if(nums[nums.length -1] < target) return nums.length;
 		int left = 0;
 		int right = nums.length -1;
-		while(left < right) {
+		while(left <= right) {
 			int mid = left + ((right - left) >> 1 );
 			if(nums[mid] == target) return mid;
 			else if(nums[mid] < target) left = mid + 1;
-			else right  = mid ;
+			else right  = mid - 1 ;
 		}
-		return right;
+		return left;
     }
 }
