4df677dfa5b9fd21acadd99fd9836ee85429eeed
MarsForever <wuguijiadeyu@gmail.com>
2018-12-21 00:13:26 +0900

    add 54 657

--- src/Solutions/Solution54.java
+++ src/Solutions/Solution54.java
@@ -8,6 +8,8 @@ public class Solution54 {
 		List<Integer> res = new ArrayList<Integer>();
 		for(int i = 0 ; i < matrix.length ; i++) {
 			for(int j = 0 ; j < matrix[0].length ; j++) {
+				if(j > matrix[0].length)
+					i++;
 				res.add(matrix[i][j]);
 			}
 		}
