dfef55c76d8712aca657ba89c5ddbeea3b3f114b
MarsForever <wuguijiadeyu@gmail.com>
2018-12-25 23:52:35 +0900

    2 ms, faster than 36.93% of Java online submissions for Spiral Matrix.

--- src/Execute/spiralMatrix54.java
+++ src/Execute/spiralMatrix54.java
@@ -1,7 +1,13 @@
 package Execute;
 
+import Solutions.Solution54;
+
 public class spiralMatrix54 {
 	public static void main(String args[]) {
-		
+		int[][] matrix ={{1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 }};
+		int[][] matrix2 ={{1, 2, 3, 4 },{ 5, 6, 7, 8},{ 9,10,11,12}};
+		Solution54 test = new Solution54();
+		System.out.println(test.spiralOrder(matrix));
+		System.out.println(test.spiralOrder(matrix2));
 	}
 }
--- src/Solutions/Solution54.java
+++ src/Solutions/Solution54.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 public class Solution54 {
 	public List<Integer> spiralOrder(int[][] matrix) {
-		List<Integer> res = new ArrayList<Integer>();
-		for(int i = 0 ; i < matrix.length ; i++) {
-			for(int j = 0 ; j < matrix[0].length ; j++) {
-				if(j > matrix[0].length)
-					i++;
-				res.add(matrix[i][j]);
-			}
-		}
-		return res ;
+        List ans = new ArrayList();
+        if (matrix.length == 0) return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+//        https://leetcode.com/problems/spiral-matrix/solution/   
+//        Approach 1: Simulation
+//        [0][1] is to the right, [1][0] is down, [0][-1] is to the left, and [-1][0] is up.
+//           0   is to the right,      1 is down,     2   is to the left,        3    is up. 
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+          if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
+            		r = cr;
+                c = cc;
+            } else {
+//	retate all 4 directions right,down,left,up 
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
     }
 }
