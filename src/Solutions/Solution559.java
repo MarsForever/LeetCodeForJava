67659084ddd010c5e4be0e4b4e9e8faaa2e5682e
MarsForever <wuguijiadeyu@gmail.com>
2019-06-08 01:39:04 +0900

    Your runtime beats 99.15 % of java submissions. Your memory usage beats 59.75 % of java submissions.

---
+++ src/Solutions/Solution559.java
@@ -0,0 +1,50 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import Execute.Node;
+
+public class Solution559 {
+
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        int res = 1;
+        for (Node child : root.children) {
+            res = Math.max(res, maxDepth(child) + 1);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Node child11 = new Node(11,null);
+        Node child12 = new Node(12,null);
+
+        Node child21 = new Node(21,null);
+
+        ArrayList ll =new ArrayList<>();
+        ll.add(new Node(222,null));
+
+        Node child22 = new Node(22,ll);
+        ll =new ArrayList<>();
+        ll.add(child11);
+        ll.add(child12);
+        Node child1 = new Node(1,ll);
+
+        ll =new ArrayList<>();
+        ll.add(child21);
+        ll.add(child22);
+        Node child2 = new Node(2,ll);
+
+        ll =new ArrayList<>();
+        ll.add(child1);
+        ll.add(child2);
+        Node root = new Node(10,ll);
+//            10
+//      1            2
+//    11  12     21       22
+//                     222
+        Solution559 test = new Solution559();
+        System.out.println(test.maxDepth(root));
+
+    }
+}
